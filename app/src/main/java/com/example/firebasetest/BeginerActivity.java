14f85d4d9b7f265883308a7a89c47d088e75b358
Maia <pcelica17@gmail.com>
2020-12-17 03:21:22 +0100

    napravila

---
+++ app/src/main/java/com/example/firebasetest/BeginerActivity.java
@@ -0,0 +1,14 @@
+package com.example.firebasetest;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class BeginerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_beginer);
+    }
+}
--- app/src/main/java/com/example/firebasetest/View/MainActivity.java
+++ app/src/main/java/com/example/firebasetest/View/MainActivity.java
@@ -7,13 +7,15 @@ import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.example.firebasetest.PopActivity;
 import com.example.firebasetest.R;
 
 public class MainActivity extends AppCompatActivity {
 
     Button button;
     Button button1;
-    
+    Button button2;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,6 +23,7 @@ public class MainActivity extends AppCompatActivity {
 
         button = (Button) findViewById(R.id.button);
         button1 = (Button) findViewById(R.id.button1);
+        button2 = (Button) findViewById(R.id.button2);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -31,6 +34,13 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 openActivity3();
+            }
+        });
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(getApplicationContext(), PopActivity.class);
+                startActivity(i);
             }
         });
     }
